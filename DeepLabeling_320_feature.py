8d9bcd3b0d3f31697eaf68ed9f70b5303967190a
tmquan <quantm@unist.ac.kr>
2018-04-03 14:51:05 -0400

    to topaz

---
+++ DeepLabeling_320_feature.py
@@ -0,0 +1,204 @@
+from Utility import * 
+
+
+class Model(ModelDesc):
+	#FusionNet
+	@auto_reuse_variable_scope
+	def generator(self, img, last_dim=1):
+		assert img is not None
+		return arch_generator(img, last_dim=last_dim)
+		# return arch_fusionnet(img)
+
+	@auto_reuse_variable_scope
+	def discriminator(self, img):
+		assert img is not None
+		return arch_discriminator(img)
+
+
+	def inputs(self):
+		return [
+			tf.placeholder(tf.float32, (DIMZ, DIMY, DIMX, 1), 'image_p'),
+			tf.placeholder(tf.float32, (DIMZ, DIMY, DIMX, 1), 'membr_p'),
+			tf.placeholder(tf.float32, (DIMZ, DIMY, DIMX, 1), 'label_p'),
+			]
+
+	def build_graph(self, image_p, membr_p, label_p):
+		G = tf.get_default_graph() # For round
+
+		pi, pm, pl = image_p, membr_p, label_p #inputs
+
+		pi = tf_2tanh(pi)
+		pm = tf_2tanh(pm)
+		pl = tf_2tanh(pl)
+
+		with tf.variable_scope('gen'):
+			with tf.variable_scope('lbl'):
+				pif, _  = self.generator(pi, last_dim=NB_FILTERS) #32
+
+		# with G.gradient_override_map({"Round": "Identity", "SegToAff": "Identity", "AffToSeg": "Identity"}):
+		# 	pa   = tf_2tanh(seg_to_aff_op(toMaxLabels(pl, factor=MAX_LABEL),   name='pa'), maxVal=1.0) # Calculate the affinity 	#0, 1
+		# 	pila = tf_2tanh(seg_to_aff_op(toMaxLabels(pil, factor=MAX_LABEL),   name='pila'), maxVal=1.0) # Calculate the affinity 	#0, 1
+
+		# pil  = tf.identity(pil, name='pil')
+		# pila = tf.identity(pila, name='pila')
+		# Calculate the loss
+		losses = []
+		# with tf.name_scope('rand_loss'):
+		# 	rand_il  = tf.reduce_mean(tf_rand_score(toMaxLabels(pl,   factor=MAX_LABEL), 
+		# 											 toMaxLabels(pil, factor=MAX_LABEL)), name='rand_il')
+		# 	losses.append(rand_il)
+		# 	add_moving_summary(rand_il)
+
+		# with tf.name_scope('reg_loss'):
+		# 	reg_il   = regDLF(toMaxLabels(pl,   factor=MAX_LABEL), toMaxLabels(pil , factor=MAX_LABEL), name='reg_il')
+			
+		# 	losses.append(reg_il)
+		# 	add_moving_summary(reg_il)
+			
+		# with tf.name_scope('aff_loss'):		
+		# 	aff_ila  = tf.identity(tf.subtract(binary_cross_entropy(tf_2imag(pa, maxVal=1.0), tf_2imag(pila, maxVal=1.0)), 
+		# 			    		 			  dice_coe(tf_2imag(pa, maxVal=1.0), tf_2imag(pila, maxVal=1.0), axis=[0,1,2,3], loss_type='jaccard')),
+		# 						 name='aff_ila')
+		# 	losses.append(3e-3*aff_ila)
+		# 	add_moving_summary(aff_ila)
+
+		# with tf.name_scope('abs_loss'):		
+		# 	abs_ila = tf.reduce_mean(tf.abs(pa - pila), name='abs_ila')
+		# 	losses.append(abs_ila)
+		# 	add_moving_summary(abs_ila)	
+
+		# 	abs_il = tf.reduce_mean(tf.abs(pl - pil), name='abs_il')
+		# 	losses.append(abs_il)
+		# 	add_moving_summary(abs_il)	
+
+		with tf.name_scope('discrim_loss'):
+			discrim_if  = regDLF(toMaxLabels(pl,   factor=MAX_LABEL), pif, name='discrim_il')
+			GAMMA = 1e1
+			losses.append(GAMMA*discrim_if)
+			add_moving_summary(discrim_if)
+
+		self.cost = tf.reduce_sum(losses, name='self.cost')
+		add_moving_summary(self.cost)	
+		# Visualization
+		# Segmentation
+		pz = tf.zeros_like(pi)
+		viz = tf.concat([tf.concat([pi, pl, pz, pz, pz, pz, pz, pz], 2), 
+						 tf.concat([pif[:,:,:,0:1], pif[:,:,:,1:2], pif[:,:,:,2:3], pif[:,:,:,3:4], pif[:,:,:,4:5], pif[:,:,:,5:6], pif[:,:,:,6:7], pif[:,:,:,7:8], ], 2), 
+						 tf.concat([pif[:,:,:,8:9], pif[:,:,:,9:10], pif[:,:,:,10:11], pif[:,:,:,11:12], pif[:,:,:,12:13], pif[:,:,:,13:14], pif[:,:,:,14:15], pif[:,:,:,15:16], ], 2), 
+						 tf.concat([pif[:,:,:,16:17], pif[:,:,:,17:18], pif[:,:,:,18:19], pif[:,:,:,19:20], pif[:,:,:,20:21], pif[:,:,:,21:22], pif[:,:,:,22:23], pif[:,:,:,23:24], ], 2), 
+						 tf.concat([pif[:,:,:,24:25], pif[:,:,:,25:26], pif[:,:,:,26:27], pif[:,:,:,27:28], pif[:,:,:,28:29], pif[:,:,:,29:30], pif[:,:,:,30:31], pif[:,:,:,31:32], ], 2), 
+						 ], 1)
+
+		viz = tf_2imag(viz)
+		viz = tf.cast(tf.clip_by_value(viz, 0, 255), tf.uint8, name='viz')
+		tf.summary.image('colorized', viz, max_outputs=50)
+
+
+	def optimizer(self):
+		lr = symbolic_functions.get_scalar_var('learning_rate', 2e-4, summary=True)
+		return tf.train.AdamOptimizer(lr, beta1=0.5, epsilon=1e-3)
+
+###############################################################################
+class VisualizeRunner(Callback):
+	def _setup_graph(self):
+		self.pred = self.trainer.get_predictor(
+			['image_p', 'membr_p', 'label_p'], ['viz'])
+
+	def _before_train(self):
+		global args
+		self.test_ds = get_data(args.data, isTrain=False, isValid=False, isTest=True)
+
+	def _trigger(self):
+		for lst in self.test_ds.get_data():
+			viz_test = self.pred(lst)
+			viz_test = np.squeeze(np.array(viz_test))
+
+			#print viz_test.shape
+
+			self.trainer.monitors.put_image('viz_test', viz_test)
+###############################################################################
+def get_data(dataDir, isTrain=False, isValid=False, isTest=False):
+	# Process the directories 
+	if isTrain:
+		num=500
+		names = ['trainA', 'trainB']
+	if isValid:
+		num=1
+		names = ['trainA', 'trainB']
+	if isTest:
+		num=1
+		names = ['testA', 'testB']
+
+	
+	dset  = ImageDataFlow(os.path.join(dataDir, names[0]),
+						  os.path.join(dataDir, names[1]),
+						  num, 
+						  isTrain=isTrain, 
+						  isValid=isValid, 
+						  isTest =isTest)
+	dset.reset_state()
+	return dset
+###############################################################################
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--gpu',    	default='0', help='comma seperated list of GPU(s) to use.')
+	parser.add_argument('--data',  default='data/Kasthuri15/3D/', required=True, 
+									help='Data directory, contain trainA/trainB/validA/validB')
+	parser.add_argument('--load',   help='Load the model path')
+	parser.add_argument('--sample', help='Run the deployment on an instance',
+									action='store_true')
+
+	args = parser.parse_args()
+	# python Exp_FusionNet2D_-VectorField.py --gpu='0' --data='arranged/'
+
+	
+	train_ds = get_data(args.data, isTrain=True, isValid=False, isTest=False)
+	valid_ds = get_data(args.data, isTrain=False, isValid=True, isTest=False)
+	# test_ds  = get_data(args.data, isTrain=False, isValid=False, isTest=True)
+
+
+	train_ds  = PrefetchDataZMQ(train_ds, 24)
+	train_ds  = PrintData(train_ds)
+	# train_ds  = QueueInput(train_ds)
+	model 	  = Model()
+
+	os.environ['PYTHONWARNINGS'] = 'ignore'
+
+	# Set the GPU
+	if args.gpu:
+		os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+	# Running train or deploy
+	if args.sample:
+		# TODO
+		# sample
+		pass
+	else:
+		# Set up configuration
+		# Set the logger directory
+		logger.auto_set_dir()
+
+		# session_init = SaverRestore(args.load) if args.load else None, 
+
+		# Set up configuration
+		config = TrainConfig(
+			model           =   model, 
+			dataflow        =   train_ds,
+			callbacks       =   [
+				PeriodicTrigger(ModelSaver(), every_k_epochs=50),
+				PeriodicTrigger(VisualizeRunner(), every_k_epochs=5),
+				# PeriodicTrigger(InferenceRunner(ds_valid, [ScalarStats('loss_membr')]), every_k_epochs=5),
+				# ScheduledHyperParamSetter('learning_rate', [(0, 2e-4), (100, 1e-4), (200, 1e-5), (300, 1e-6)], interp='linear')
+				ScheduledHyperParamSetter('learning_rate', [(0, 1e-6), (300, 1e-6)], interp='linear')
+				# ScheduledHyperParamSetter('learning_rate', [(30, 6e-6), (45, 1e-6), (60, 8e-7)]),
+				# HumanHyperParamSetter('learning_rate'),
+				],
+			max_epoch       =   500, 
+			session_init    =    SaverRestore(args.load) if args.load else None,
+			#nr_tower        =   max(get_nr_gpu(), 1)
+			)
+	
+		# Train the model
+		SyncMultiGPUTrainer(config).train()
+		# trainer = SyncMultiGPUTrainerReplicated(max(get_nr_gpu(), 1))
+		# launch_train_with_config(config, trainer)
